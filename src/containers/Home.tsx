c9148fa29dd6f85b8752c21912699fb1d19a9997
koog7 <k.ognev709@mail.com>
2024-07-10 13:14:34 +0600

    Сделан примерный макет домофона

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-
 const Home = () => {
+
     return (
         <div>
-            123
+            <div className="container">
+                <div className="display">{1337}</div>
+                <div className="button-grid">
+                    <button className="button">7</button>
+                    <button className="button">8</button>
+                    <button className="button">9</button>
+                    <button className="button">4</button>
+                    <button className="button">5</button>
+                    <button className="button">6</button>
+                    <button className="button">1</button>
+                    <button className="button">2</button>
+                    <button className="button">3</button>
+                    <button className="button">{'<'}</button>
+                    <button className="button">0</button>
+                    <button className="button">E</button>
+                </div>
+            </div>
         </div>
     );
 };
