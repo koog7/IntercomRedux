c46f31f7c051c1590d79562aa56ff69cead087e7
koog7 <k.ognev709@mail.com>
2024-07-11 11:47:44 +0600

    Добавлена функция проверки пинкода и выводом сообщения и цвета

--- src/containers/Counter/CounterSlice.tsx
+++ src/containers/Counter/CounterSlice.tsx
@@ -2,10 +2,14 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 interface CounterState {
     value: string;
+    message: string;
+    color: string;
 }
 
 const initialState: CounterState = {
     value: '',
+    message: '',
+    color: '',
 };
 
 export const counterSlice = createSlice({
@@ -20,8 +24,12 @@ export const counterSlice = createSlice({
         clearArea: (state) => {
             state.value = state.value.slice(0, -1);
         },
+        enter: (state, action: PayloadAction<{ message: string, color: string }>) => {
+            state.message = action.payload.message;
+            state.color = action.payload.color;
+        },
     },
 });
 
 export const counterReducer = counterSlice.reducer;
-export const  {addNum, clearArea}  = counterSlice.actions;
+export const  {addNum, clearArea,enter}  = counterSlice.actions;
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,22 +1,34 @@
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../app/store.ts";
-import {addNum, clearArea} from "./Counter/CounterSlice.tsx";
+import {addNum, clearArea, enter} from "./Counter/CounterSlice.tsx";
 
 const Home = () => {
 
-    const value = useSelector((state: RootState) => state.counter.value);
     const dispatch = useDispatch<AppDispatch>();
-
+    const value = useSelector((state: RootState) => state.counter.value);
+    const message = useSelector((state: RootState) => state.counter.message);
+    const color = useSelector((state: RootState) => state.counter.color);
+    const numlock = '1337';
     const addNumber = (value: string) => {
         dispatch(addNum(value));
     };
     const clear = () => {
         dispatch(clearArea())
     }
+
+    const enterNum = () => {
+        if (value === numlock) {
+            dispatch(enter({ message: 'Granted', color: 'green' }));
+        } else {
+            dispatch(enter({ message: 'Denied', color: 'red' }));
+        }
+    };
+
     return (
         <div>
             <div className="container">
-                <div className="display">{value}</div>
+                <div style={{border: `1px solid ${color ? color : 'gray'}`}} className="display">{value}</div>
+                <div style={{padding:'5px 0 10px 0'}}>Access: {message ? message : '.............'}</div>
                 <div className="button-grid">
                     {['7', '8', '9', '4', '5', '6', '1', '2', '3'].map((num) => (
                         <button key={num} className="button" onClick={() => addNumber(num)}>
@@ -25,7 +37,7 @@ const Home = () => {
                     ))}
                     <button className="button" onClick={clear}>{'<'}</button>
                     <button className="button" onClick={() => addNumber('0')}>0</button>
-                    <button className="button">E</button>
+                    <button className="button" onClick={enterNum}>E</button>
                 </div>
             </div>
         </div>
